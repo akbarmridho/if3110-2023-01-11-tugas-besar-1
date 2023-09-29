8df1d5a4815a81543dbd03a40e73baa0fdfe6b5a
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-09-29 20:19:44 +0700

    feat: autoloader

---
+++ Core/Autoloader.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Core;
+
+class Autoloader
+{
+    public static function register()
+    {
+        spl_autoload_register(function ($class) {
+            $file = ".." . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+
+            if (file_exists($file)) {
+                require $file;
+                return true;
+            }
+            return false;
+        });
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
 
-echo "Hello world";
+// Register autoloader
+require __DIR__ . '/../Core/Autoloader.php';
+
+\Core\Autoloader::register();
+
+\App\Hello::run();
