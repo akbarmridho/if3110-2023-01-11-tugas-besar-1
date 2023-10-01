39bb11ecba70ce2287798f4786505ee2c770dea7
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2023-10-01 17:00:20 +0700

    feat: ddl cascade on delete

--- migrations/listwibuku.sql
+++ migrations/listwibuku.sql
@@ -1,3 +1,4 @@
+-- parent table: Anime
 CREATE TABLE IF NOT EXISTS Anime (
     id SERIAL PRIMARY KEY,
     
@@ -14,21 +15,24 @@ CREATE TABLE IF NOT EXISTS Anime (
 );
 
 CREATE TABLE IF NOT EXISTS Anime_Poster (
-    anime_id INTEGER REFERENCES Anime(id),
+    anime_id INTEGER REFERENCES Anime(id) ON DELETE CASCADE,
     poster TEXT,
     PRIMARY KEY(anime_id, poster)
 );
 
 CREATE TABLE IF NOT EXISTS Anime_Trailer (
-    anime_id INTEGER REFERENCES Anime(id),
+    anime_id INTEGER REFERENCES Anime(id) ON DELETE CASCADE,
     trailer TEXT,
     PRIMARY KEY(anime_id, trailer)
 );
+
 
 -- USER_ROLE Type
 DROP TYPE IF EXISTS USER_ROLE;
 CREATE TYPE USER_ROLE AS ENUM ('ADMIN', 'USER');
 
+
+-- parent table: User_Data
 CREATE TABLE IF NOT EXISTS User_Data (
     id SERIAL PRIMARY KEY,
     name TEXT,
@@ -41,21 +45,22 @@ CREATE TABLE IF NOT EXISTS User_Data (
     updated_at TIMESTAMP DEFAULT NOW()
 );
 
+
 -- WATCH_STATUS Type
 DROP TYPE IF EXISTS WATCH_STATUS;
 CREATE TYPE WATCH_STATUS AS ENUM ('WISHLIST', 'WATCHING', 'WATCHED');
 
 
 CREATE TABLE IF NOT EXISTS User_Anime (
-    user_id INTEGER REFERENCES User_Data(id),
-    anime_id INTEGER REFERENCES Anime(id),
+    user_id INTEGER REFERENCES User_Data(id) ON DELETE CASCADE,
+    anime_id INTEGER REFERENCES Anime(id) ON DELETE CASCADE,
     status WATCH_STATUS DEFAULT 'WISHLIST',
     PRIMARY KEY(user_id, anime_id)
 );
 
 CREATE TABLE IF NOT EXISTS Review (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES User_Data(id),
+    user_id INTEGER REFERENCES User_Data(id) ON DELETE CASCADE,
     review TEXT,
     rating INTEGER CHECK (rating >= 1 AND rating <= 10),
 
