913bad7349b80d83aa0201318ed5965d6a6db0bb
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2023-10-05 20:39:39 +0700

    todo: js script for anime-stats

---
+++ resources/js/components/animestats.js
@@ -0,0 +1,28 @@
+/* ANIMESTATS */
+/* (watch_status, rating) */
+
+const statusSelect = document.getElementById('select-watch-status')
+const statusOptions = statusSelect.querySelectorAll('.watch-status-option')
+statusOptions.forEach(option => {
+    option.addEventListener(onClickStatusOption)
+    // todo
+});
+
+const ratingSelect = document.getElementById('select-rating')
+const ratingOptions = ratingSelect.querySelectorAll('.rating-option')
+ratingOptions.forEach(option => {
+    // todo
+})
+
+
+function onClickStatusOption() {
+    
+}
+
+function onClickRatingOption() {
+    
+}
+
+
+
+
--- resources/views/components/anime-stats/animestats.view.php
+++ resources/views/components/anime-stats/animestats.view.php
@@ -1,19 +1,21 @@
 <?php if (isset($user_anime)) : ?>
     <!-- todo: delete and add from list -->
     <a href='#' class='btn btn-primary btn-small'>Delete from list</a>
+    
     <select name="select-watch-status" id="select-watch-status" class="select-stats">
         <?php foreach(array('WISHLIST', 'WATCHING', 'WATCHED') as $watchStatus) : ?>
             <!-- todo: update on selection update -->
-            <option value='<?= $watchStatus?>' <?= $user_anime->status == $watchStatus ? 'selected' : '' ?>><?= ucfirst(strtolower($watchStatus))?></option>
+            <option class='watch-status-option' value='<?= $watchStatus?>' <?= $user_anime->status == $watchStatus ? 'selected' : '' ?>><?= ucfirst(strtolower($watchStatus))?></option>
         <?php endforeach ?>
     </select>
 <? else: ?>
     <a href='#' class='btn btn-primary btn-small'>Add to list</a>
 <? endif ?> 
+
 <select name="select-rating" id="select-rating" class="select-stats" <?= isset($user_anime) ? '' : 'disabled'?>>
     <!-- todo: update on selection update -->
-    <option value="0">Select Rating</option>
+    <option class='rating-option' value="0">Select Rating</option>
     <?php for ($i=1; $i <= 10; $i++) : ?>
-        <option value='<?= $i?>' <?= isset($review) ? ($review->rating == $i ? 'selected' : '') : ''?> ><?= $i?> / 10</option>
+        <option class='rating-option' value='<?= $i?>' <?= isset($review) ? ($review->rating == $i ? 'selected' : '') : ''?> ><?= $i?> / 10</option>
     <?php endfor ?>
 </select>
