be88515f2a37c83be9fd797206f64f4650fddf73
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2023-10-09 12:34:01 +0700

    feat: update poster, trailer

--- App/Http/Controller/EditAnimeController.php
+++ App/Http/Controller/EditAnimeController.php
@@ -6,9 +6,12 @@ use App\Model\Anime;
 use Core\Http\Request;
 use Core\Base\BaseController;
 use Core\Session\Session;
+use Core\Validator\Rules\ImageRule;
+use Core\Validator\Rules\VideoRule;
 use Core\Validator\Types\IntType;
 use Core\Validator\Types\StringType;
 use Core\Validator\Types\TimestampType;
+use Core\Validator\Types\UploadedFileType;
 use Core\Validator\Validator;
 
 class EditAnimeController extends BaseController
@@ -28,12 +31,41 @@ class EditAnimeController extends BaseController
             'description' => new StringType(required: false, nullable: true),
             'episode_count' => new IntType(required: false, nullable: true, shouldCast: true),
             'air_date_start' => new TimestampType(required: false, nullable: true),
-            'air_date_end' => new TimestampType(required: false, nullable: true)
+            'air_date_end' => new TimestampType(required: false, nullable: true),
+            'poster' => new UploadedFileType(required: false, rules: [new ImageRule()]),
+            'trailer' => new UploadedFileType(required: false, rules: [new VideoRule()])
         ]);
 
         if ($validated->isError()) {
             render('editanime', ['anime' => Anime::findById($request->getRouteParam('id'))]);
         } else {
+
+            if (array_key_exists('poster', $validated->data)) {
+
+                if ($validated->data['poster']['name'] !== '') {
+                    // handle image upload
+                    $poster = $validated->data['poster'];
+                    // todo: remove previous image
+                    $validated->data['poster'] = move_uploaded_to_storage($poster['tmp_name'], $poster['name']);
+                } else {
+                    // need to check name is not empty because if empty then no image exist
+                    unset($validated->data['poster']);
+                }
+            }
+
+            if (array_key_exists('trailer', $validated->data)) {
+
+                if ($validated->data['trailer']['name'] !== '') {
+                    // handle video upload
+                    $trailer = $validated->data['trailer'];
+                    // todo: remove previous video
+                    $validated->data['trailer'] = move_uploaded_to_storage($trailer['tmp_name'], $trailer['name']);
+                } else {
+                    // need to check name is not empty because if empty then no image exist
+                    unset($validated->data['trailer']);
+                }
+            }
+
             $rowCount = Anime::update($request->getRouteParam('id'), $validated->data);
             if ($rowCount == 0) {
                 render('editanime', ['error' => 'Failed to add new anime']);
